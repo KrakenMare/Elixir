f2e133ceaaef7a7d5e7eb9a1b695c1f3be594000
KrakenMare <Arcanuo123@gmail.com>
2017-03-16 10:20:27 -0700

    Add files via upload

--- dbHandler.php
+++ dbHandler.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $user = 'root';
-$pass = 'LoganBarber';
+$pass = 'Lokeybokey';
 $db = 'rotmgprod';
 $conn = new mysqli($host, $user, $pass, $db);
     if ($conn->connect_error)
--- success.php
+++ success.php
@@ -5,9 +5,9 @@ $subject = $playername . "'S donation on" . $servername;
 $message = "Thanks for donating and supporting" . $servername . " If you don't have your donation within twelve hours! Contact an Administrator!";
 $headers = 'From: webmaster@example.com' . "\r\n";
 $email = $_GET['email'];
-$rank = $_GET['item_number'];
+$rank = $_GET['item_number']; // The itemnumber from the thing.
 $playername = $_GET['playername'];
-$statement = "UPDATE accounts SET rank='2' WHERE name='$playername'";
+$statement = "UPDATE accounts SET rank='$rank' WHERE name='$playername'";
 if ($conn->query($statement) === TRUE) {
     echo "You've been ranked! A recipt will be emailed!";
     echo $email;
